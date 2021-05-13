1b9e4fb33af81f59b4ed1f120a07e7bd1f655c5b
Uliana171 <ulianakonyk2104@gmail.com>
2021-05-13 12:50:08 +0200

    add module4/7

---
+++ js/module4.js
@@ -0,0 +1,135 @@
+// 1
+// Дополни код так, чтобы в переменной result
+// был результат выполнения функции makePizza,
+// а в переменной pointer была ссылка на функцию
+// makePizza.
+
+function makePizza() {
+  return 'Ваша пицца готовится, ожидайте.';
+}
+// Пиши код ниже этой строки
+
+const result = makePizza();
+const pointer = makePizza;
+
+// 2
+function deliverPizza(pizzaName) {
+  return `Доставляем пиццу ${pizzaName}.`;
+}
+
+function makePizza(pizzaName) {
+  return `Пицца ${pizzaName} готовится, ожидайте...`;
+}
+
+// Пиши код ниже этой строки
+function makeMessage(pizzaName, callback) {
+  return callback(pizzaName);
+}
+
+// 3
+function makePizza(pizzaName, callback) {
+  console.log(`Пицца ${pizzaName} готовится, ожидайте...`);
+  callback(pizzaName);
+}
+
+makePizza('Роял гранд', function deliverPizza(pizzaName) {
+  console.log(`Доставляем пиццу ${pizzaName}.`);
+});
+// Пиши код ниже этой строки
+
+makePizza('Ультрасыр', function eatPizza(pizzaName) {
+  console.log( `Едим пиццу ${pizzaName}.`)
+});
+
+
+
+// 4
+const pizzaPalace = {
+  pizzas: ['Ультрасыр', 'Аль Копчино', 'Четыре нарезона'],
+  order(pizzaName, onSuccess, onError) {
+
+    if (this.pizzas.includes(pizzaName)) {
+      return onSuccess(pizzaName);
+
+    }
+
+    return onError(`В ассортименте нет пиццы с названием ${pizzaName}.`);
+
+  },
+};
+// Пиши код выше этой строки
+
+// Колбэк для onSuccess
+function makePizza(pizzaName) {
+  return `Ваш заказ принят. Готовим пиццу ${pizzaName}.`;
+}
+
+// Колбэк для onError
+function onOrderError(error) {
+  return `Ошибка! ${error}`;
+}
+
+// Вызовы метода с колбэками
+pizzaPalace.order('Аль Копчино', makePizza, onOrderError);
+pizzaPalace.order('Четыре нарезона', makePizza, onOrderError);
+pizzaPalace.order('Биг майк', makePizza, onOrderError);
+pizzaPalace.order('Венская', makePizza, onOrderError);
+
+
+
+// 5
+const pizzaPalace = {
+  pizzas: ['Ультрасыр', 'Аль Копчино', 'Четыре нарезона'],
+  // Пиши код ниже этой строки
+  checkPizza(pizzaName) {
+    return this.pizzas.includes(pizzaName);
+  },
+  order(pizzaName) {
+    const isPizzaAvailable = this.checkPizza(pizzaName);
+
+    if (!isPizzaAvailable) {
+      return `В ассортименте нет пиццы с названием «${pizzaName}».`;
+    }
+
+    return `Заказ принят, готовим пиццу «${pizzaName}».`;
+  },
+  // Пиши код выше этой строки
+};
+
+
+
+// 6
+const customer = {
+  username: 'Mango',
+  balance: 24000,
+  discount: 0.1,
+  orders: ['Burger', 'Pizza', 'Salad'],
+  // Пиши код ниже этой строки
+  getBalance() {
+    return this.balance;
+  },
+  getDiscount() {
+    return this.discount;
+  },
+  setDiscount(value) {
+    this.discount = value;
+  },
+  getOrders() {
+    return this.orders;
+  },
+  addOrder(cost, order) {
+    this.balance -= cost - cost * this.discount;
+    this.orders.push(order);
+  },
+  // Пиши код выше этой строки
+};
+
+customer.setDiscount(0.15);
+console.log(customer.getDiscount()); // 0.15
+customer.addOrder(5000, 'Steak');
+console.log(customer.getBalance()); // 19750
+console.log(customer.getOrders()); // ['Burger', 'Pizza', 'Salad', 'Steak']
+ 
+
+
+// 7
--- js/study.js
+++ js/study.js
@@ -231,3 +231,23 @@ const friends = ['Mango', 'Kiwi', 'Poly', 'Ajax'];
 
 
 
+
+
+const orders = [
+  { email: 'solomon@topmail.ua', dish: 'Burger' },
+  { email: 'artemis@coldmail.net', dish: 'Pizza' },
+  { email: 'jacob@mail.com', dish: 'Taco' },
+];
+// Пиши код ниже этой строки
+function composeMessage(position) {
+  return `Готовим ${this.dish} для 
+  ${this.email}. Ваш заказ ${position}-й в очереди.`
+};
+const messages = [];
+for (let i = 0; i < orders.length; i ++) {
+  const message = composeMessage.call(orders[i], i+1)
+  messages.push(message)
+};
+console.log(messages);
+
+  
